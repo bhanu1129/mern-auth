c36459668557b6c4073cd51fba30073285d7c237
bhanu1129 <bhanubansal913@gmail.com>
2023-10-12 14:26:52 +0530

    Setting up mongodb and creating user model

--- api/index.js
+++ api/index.js
@@ -1,4 +1,14 @@
 const express = require('express');
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+
+dotenv.config();
+
+mongoose.connect(process.env.MONGO).then(()=>{
+    console.log('Connected to MongoDB')
+}).catch((err)=>{
+    console.error(err);
+})
 
 const app = express();
 
