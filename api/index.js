b168d81aeddca1529655097b6e49e7bcb05c14cd
bhanu1129 <bhanubansal913@gmail.com>
2023-10-12 23:24:37 +0530

    signup api route

--- api/index.js
+++ api/index.js
@@ -1,20 +1,26 @@
-const express = require('express');
-const mongoose = require('mongoose');
-const dotenv = require('dotenv');
-const userRoutes = require('./routes/user.route.js');
-
+const express = require("express");
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+const userRoutes = require("./routes/user.route.js");
+const authRoutes = require("./routes/auth.route.js");
 dotenv.config();
 
-mongoose.connect(process.env.MONGO).then(()=>{
-    console.log('Connected to MongoDB')
-}).catch((err)=>{
+mongoose
+  .connect(process.env.MONGO)
+  .then(() => {
+    console.log("Connected to MongoDB");
+  })
+  .catch((err) => {
     console.error(err);
-})
+  });
 
 const app = express();
 
-app.listen(3000, ()=>{
-    console.log("Server listening on port 3000!");
+app.use(express.json());
+
+app.listen(3000, () => {
+  console.log("Server listening on port 3000!");
 });
 
 app.use("/api/user", userRoutes);
+app.use("/api/auth", authRoutes);
