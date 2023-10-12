9c9d3b3dcafd0834f22e28fb7a09579afbbecf68
bhanu1129 <bhanubansal913@gmail.com>
2023-10-12 14:58:30 +0530

    test api route

--- api/index.js
+++ api/index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const dotenv = require('dotenv');
+const userRoutes = require('./routes/user.route.js');
 
 dotenv.config();
 
@@ -15,3 +16,5 @@ const app = express();
 app.listen(3000, ()=>{
     console.log("Server listening on port 3000!");
 });
+
+app.use("/api/user", userRoutes);
