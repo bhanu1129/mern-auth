4251e34b5330daaf239a3d640fc88433a5984eda
bhanu1129 <bhanubansal913@gmail.com>
2023-10-30 23:28:13 +0530

    make profile private

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,6 +1,9 @@
 import { Link } from "react-router-dom";
+import { useSelector } from "react-redux";
 
 export default function Header() {
+  const { currentUser } = useSelector((state) => state.user);
+
   return (
     <div className="bg-slate-200">
       <div
@@ -17,8 +20,12 @@ export default function Header() {
           <Link to="/about">
             <li>About</li>
           </Link>
-          <Link to="/sign-in">
-            <li>Sign In</li>
+          <Link to="/profile">
+            {currentUser ? (
+              <img src={currentUser.profilePicture} alt="profile" className="h-7 w-7 rounded-full object-cover"/>
+            ) : (
+              <li>Sign In</li>
+            )}
           </Link>
         </ul>
       </div>
